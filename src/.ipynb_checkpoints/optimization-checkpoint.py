72a9a278f1d04184f04ff2a0e9f8b529b68d2f12
Cyril <kwakucyril@gmail.com>
2023-10-17 00:06:31 +0000

    optimizers

--- src/.ipynb_checkpoints/optimization-checkpoint.py
+++ src/.ipynb_checkpoints/optimization-checkpoint.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim
 
 
-def get_loss():
+def get_loss(use_cuda=False):
     """
     Get an instance of the CrossEntropyLoss (useful for classification),
     optionally moving it to the GPU if use_cuda is set to True
@@ -11,6 +11,8 @@ def get_loss():
 
     # YOUR CODE HERE: select a loss appropriate for classification
     loss  = nn.CrossEntropyLoss()# YOUR CODE HERE
+    if use_cuda and torch.cuda.is_available():
+        loss = loss.cuda()
 
     return loss
 
@@ -32,19 +34,19 @@ def get_optimizer(
     :param weight_decay: regularization coefficient
     """
     if optimizer.lower() == "sgd":
-        # YOUR CODE HERE: create an instance of the SGD
-        # optimizer. Use the input parameters learning_rate, momentum
-        # and weight_decay
         opt = torch.optim.SGD(
-            # YOUR CODE HERE
+            model.parameters(),
+            lr=learning_rate,
+            momentum=momentum,
+            weight_decay=weight_decay
         )
 
     elif optimizer.lower() == "adam":
-        # YOUR CODE HERE: create an instance of the Adam
-        # optimizer. Use the input parameters learning_rate, momentum
-        # and weight_decay
         opt = torch.optim.Adam(
-            # YOUR CODE HERE
+            model.parameters(),
+            lr=learning_rate,
+            betas=(momentum, 0.999),
+            weight_decay=weight_decay
         )
     else:
         raise ValueError(f"Optimizer {optimizer} not supported")
